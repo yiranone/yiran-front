624d7d54d106acfc30da31b243166f6cd41320af
jimmy <jimmy@izhong>
2022-05-18 19:22:46 +0800

    no cdn

--- vue.config.js
+++ vue.config.js
@@ -21,15 +21,23 @@ const assetsCDN = {
     'js-cookie': 'Cookies'
   },
   css: [],
-  js2: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.3.4/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js',
-    '//cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.min.js',
-    '//cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js',
-    '//cdn.jsdelivr.net/npm/@antv/data-set@0.11.4/build/data-set.min.js',
-    '//cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js'
+  js: [
+    // '//cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js',
+    // '//cdn.jsdelivr.net/npm/vue-router@3.3.4/dist/vue-router.min.js',
+    // '//cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js',
+    // '//cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js',
+    // '//cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.min.js',
+    // '//cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js',
+    // '//cdn.jsdelivr.net/npm/@antv/data-set@0.11.4/build/data-set.min.js',
+    // '//cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js',
+    './js/vue.min.js',
+    './js/vue-router.min.js',
+    './js/vuex.min.js',
+    './js/axios.min.js',
+    './js/nprogress.min.js',
+    './js/clipboard.min.js',
+    './js/data-set.min.js',
+    './js/js.cookie.min.js'
   ]
 }
 
