9a95c51392c5b162d42e88d39ce2cf17c26bc47c
jimmy <jimmy@izhong>
2022-07-31 16:07:00 +0800

    参数配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import { blobValidate } from '@/utils/dict-utils'
 // 跨域认证信息 header 名
 const xsrfHeaderName = 'Authorization'
 
-axios.defaults.timeout = 5000
+axios.defaults.timeout = 35000
 axios.defaults.withCredentials= true
 axios.defaults.xsrfHeaderName= xsrfHeaderName
 axios.defaults.xsrfCookieName= xsrfHeaderName
@@ -19,14 +19,6 @@ axios.defaults.xsrfCookieName= xsrfHeaderName
 //     return data
 //   }
 // }]
-
-// 认证类型
-const AUTH_TYPE = {
-  BEARER: 'Bearer',
-  BASIC: 'basic',
-  AUTH1: 'auth1',
-  AUTH2: 'auth2',
-}
 
 // http method
 const METHOD = {
@@ -55,55 +47,25 @@ async function request(url, method, params, conf) {
 /**
  * 设置认证信息
  * @param auth {Object}
- * @param authType {AUTH_TYPE} 认证类型，默认：{AUTH_TYPE.BEARER}
  */
-function setAuthorization(auth, authType = AUTH_TYPE.BEARER) {
-  switch (authType) {
-    case AUTH_TYPE.BEARER:
-      Cookie.set(xsrfHeaderName, auth.token, {expires: 7})
-      break
-    case AUTH_TYPE.BASIC:
-    case AUTH_TYPE.AUTH1:
-    case AUTH_TYPE.AUTH2:
-    default:
-      break
-  }
+function setAuthorization(auth) {
+  Cookie.set(xsrfHeaderName, auth.token, {expires: 7})
 }
 
 /**
  * 移出认证信息
- * @param authType {AUTH_TYPE} 认证类型
  */
-function removeAuthorization(authType = AUTH_TYPE.BEARER) {
-  switch (authType) {
-    case AUTH_TYPE.BEARER:
-      Cookie.remove(xsrfHeaderName)
-      break
-    case AUTH_TYPE.BASIC:
-    case AUTH_TYPE.AUTH1:
-    case AUTH_TYPE.AUTH2:
-    default:
-      break
-  }
+function removeAuthorization() {
+  Cookie.remove(xsrfHeaderName)
 }
 
 /**
  * 检查认证信息
- * @param authType
  * @returns {boolean}
  */
-function checkAuthorization(authType = AUTH_TYPE.BEARER) {
-  switch (authType) {
-    case AUTH_TYPE.BEARER:
-      if (Cookie.get(xsrfHeaderName)) {
-        return true
-      }
-      break
-    case AUTH_TYPE.BASIC:
-    case AUTH_TYPE.AUTH1:
-    case AUTH_TYPE.AUTH2:
-    default:
-      break
+function checkAuthorization() {
+  if (Cookie.get(xsrfHeaderName)) {
+    return true
   }
   return false
 }
@@ -224,7 +186,6 @@ function download (url, params, filename) {
 
 export {
   METHOD,
-  AUTH_TYPE,
   request,
   download,
   setAuthorization,
